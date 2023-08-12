74c098ac2daf4d0e81836e7d33a661565383fdb1
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-08-12 11:56:02 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.querySelector('#dodger')
+
+document.addEventListener('keydown', (e) => {
+    console.log('hello')
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
+
+
+function moveDodgerRight(e){
+ const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+function moveDodgerLeft(e){
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+
+}
